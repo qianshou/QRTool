f1085fd1e66b384ea760f98c14ffa6aca291f10a
zhezhao <799631071@qq.com>
2017-01-04 23:15:12 +0800

    修复中文编码的问题

--- javascript/index.js
+++ javascript/index.js
@@ -8,14 +8,14 @@ $(function() {
 })
 
 //chrome扩展获得当前url，并转为二维码
-chrome.tabs.query({
-    active: true,
-    currentWindow: true
-}, function(tabs){
-    var url = tabs[0].url;
-    $('#current_url').text(url);
-    createQR('current_qr',url);
-});
+// chrome.tabs.query({
+//     active: true,
+//     currentWindow: true
+// }, function(tabs){
+//     var url = tabs[0].url;
+//     $('#current_url').text(url);
+//     createQR('current_qr',url);
+// });
 
 //生成自定义二维码
 $("#create").click(function () {
@@ -34,9 +34,31 @@ $("#custom_text").click(function () {
 
 //生成二维码
 function createQR(id,content) {
+    content = utf16to8(content);
     $("#"+id).empty();
     $("#"+id).qrcode({
         render: "table",
         text: content
     });
 };
+
+//convert Chinese to utf-8
+function utf16to8(str) {
+    var out, i, len, c;
+    out = "";
+    len = str.length;
+    for(i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i);
+        } else if (c > 0x07FF) {
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+        }
+    }
+    return out;
+}
